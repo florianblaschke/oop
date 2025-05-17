44c88b86ed16c5104a9dd7a844cdd66dadc4550b
Florian Blaschke <florianblaschke@web.de>
2025-05-17 23:08:30 +0200

    add messager class

---
+++ src/messager.ts
@@ -0,0 +1,40 @@
+class Messager<TMessage extends string[] = []> {
+	private message: TMessage;
+
+	constructor() {
+		// biome-ignore lint/suspicious/noExplicitAny: any is fine and needed here. ts will yell at us at this stage and the return type gets inferred later on
+		this.message = [] as any;
+	}
+
+	add<TString extends string>(s: TString): Messager<[...TMessage, TString]> {
+		this.message.push(s);
+
+		return this as unknown as Messager<[...TMessage, TString]>;
+	}
+
+	print() {
+		return this.message.join(" ") as ConcatMessage<TMessage>;
+	}
+}
+
+type ConcatMessage<
+	TMessage extends string[],
+	TAcc extends string = "",
+> = TMessage extends [infer Word extends string, ...infer Rest extends string[]]
+	? ConcatMessage<Rest, TAcc extends "" ? Word : `${TAcc} ${Word}`>
+	: TAcc;
+
+const anotherChance = new Messager();
+
+const i_mean_that = anotherChance
+	.add("It")
+	.add("would")
+	.add("be")
+	.add("nice")
+	.add("working")
+	.add("for")
+	.add("you");
+
+const result = i_mean_that.print();
+
+console.log(result);
